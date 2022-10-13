9c3e8b1c38e6e7e5926ac0faea08923b887286bc
JoeVanCamp <vancampjoe@gmail.com>
2022-10-13 14:59:51 -0700

    setup boilerplate webpack, babel config

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+const App = () => {
+  return (
+    <h1>Hello world!</h1>
+  )
+};
+
+root.render(
+  <App />
+);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: "bundle.js"
+  },
+
+   module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      }
+    ]
+  },
+}
